f242abcf7592f9361e1571ebeea20c98b2639e56
Mark Qvist <mark@unsigned.io>
2021-05-13 16:42:36 +0200

    Version bump

--- RNS/vendor/__init__.py
+++ RNS/vendor/__init__.py
@@ -1,5 +1,7 @@
 import os
 import glob
 
+__version__ = "0.1.9"
+
 modules = glob.glob(os.path.dirname(__file__)+"/*.py")
 __all__ = [ os.path.basename(f)[:-3] for f in modules if not f.endswith('__init__.py')]
