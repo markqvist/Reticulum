95f81cab7f3c10328404b7879521b3129db88cad
Mark Qvist <mark@unsigned.io>
2021-10-08 17:09:11 +0200

    Added path expiry check to tunnel restoration

--- RNS/Transport.py
+++ RNS/Transport.py
@@ -1115,7 +1115,10 @@ class Transport:
                     else:
                         RNS.log("Did not restore path to "+RNS.prettyhexrep(packet.destination_hash)+" because a newer path with fewer hops exist", RNS.LOG_DEBUG)
                 else:
-                    should_add = True
+                    if time.time() < expires
+                        should_add = True
+                    else:
+                        RNS.log("Did not restore path to "+RNS.prettyhexrep(packet.destination_hash)+" because it has expired", RNS.LOG_DEBUG)
 
                 if should_add:
                     Transport.destination_table[destination_hash] = new_entry
