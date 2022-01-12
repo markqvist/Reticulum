b6df952995da32c13bcb09ed0b30688d7204d607
Mark Qvist <mark@unsigned.io>
2022-01-12 10:16:59 +0100

    Platform version check for Windows

--- RNS/vendor/platformutils.py
+++ RNS/vendor/platformutils.py
@@ -8,9 +8,11 @@ def get_platform():
 
 def platform_checks():
     if str(get_platform()).startswith("win32"):
+        import sys
         if sys.version_info.major >= 3 and sys.version_info.minor >= 8:
             pass
         else:
-            RNS.log("On Windows, Reticulum requires Python 3.8 or higher.")
-            RNS.log("Please update Python to run Reticulum.")
+            import RNS
+            RNS.log("On Windows, Reticulum requires Python 3.8 or higher.", RNS.LOG_ERROR)
+            RNS.log("Please update Python to run Reticulum.", RNS.LOG_ERROR)
             RNS.panic()
