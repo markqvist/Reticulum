05288d7c9794fd62993a91cb36f1adbffeb61b7d
Mark Qvist <mark@unsigned.io>
2022-01-27 23:49:24 +0100

    Updated documentation for release

--- docs/manual/_static/documentation_options.js
+++ docs/manual/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.3.1 beta',
+    VERSION: '0.3.2 beta',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, Mark Qvist'
 author = 'Mark Qvist'
 
 # The full version, including alpha/beta/rc tags
-release = '0.3.1 beta'
+release = '0.3.2 beta'
 
 
 # -- General configuration ---------------------------------------------------
