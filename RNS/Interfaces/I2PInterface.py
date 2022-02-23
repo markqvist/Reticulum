987ff0658b7dc6243d28c581a222df4482aa82a4
Mark Qvist <mark@unsigned.io>
2022-02-23 22:53:16 +0100

    Version bump

--- RNS/Interfaces/I2PInterface.py
+++ RNS/Interfaces/I2PInterface.py
@@ -34,6 +34,15 @@ class KISS():
         data = data.replace(bytes([0xc0]), bytes([0xdb, 0xdc]))
         return data
 
+# TODO: Neater shutdown of the event loop and
+# better error handling is needed. Sometimes
+# errors occur in I2P that leave tunnel setup
+# hanging indefinitely, and right now we have
+# no way of catching it. Sometimes the server
+# and client tasks are also not cancelled on
+# shutdown, which leads to errors dumped to
+# the console. This should also be remedied.
+
 class I2PController:
     def __init__(self, rns_storagepath):
         import RNS.vendor.i2plib as i2plib
