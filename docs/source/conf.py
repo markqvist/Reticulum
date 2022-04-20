3856747e31b22344e7e4b214cd54375f207234f4
Mark Qvist <mark@unsigned.io>
2022-04-20 21:25:54 +0200

    Updated docs and manual

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, Mark Qvist'
 author = 'Mark Qvist'
 
 # The full version, including alpha/beta/rc tags
-release = '0.3.4 beta'
+release = '0.3.5 beta'
 
 
 # -- General configuration ---------------------------------------------------
