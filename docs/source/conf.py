2f1329e581bccef810b8a3de4a7f8560c8e4100e
Mark Qvist <mark@unsigned.io>
2022-06-12 18:57:08 +0200

    Updated docs version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2022, Mark Qvist'
 author = 'Mark Qvist'
 
 # The full version, including alpha/beta/rc tags
-release = '0.3.7 beta'
+release = '0.3.8 beta'
 
 
 # -- General configuration ---------------------------------------------------
