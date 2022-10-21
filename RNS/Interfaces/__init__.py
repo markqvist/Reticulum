886b0ac0cab8a78f18169448ebb7c7314680099c
Mark Qvist <mark@unsigned.io>
2022-10-22 01:38:38 +0200

    Fixed Android interfaces import

--- RNS/Interfaces/__init__.py
+++ RNS/Interfaces/__init__.py
@@ -22,6 +22,7 @@
 
 import os
 import glob
+import RNS.Interfaces.Android
 
 modules = glob.glob(os.path.dirname(__file__)+"/*.py")
 __all__ = [ os.path.basename(f)[:-3] for f in modules if not f.endswith('__init__.py')]
