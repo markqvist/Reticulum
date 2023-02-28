8f0151fed61cd1e39146dc030c415c013ed191cc
Aaron Heise <5148966+acehoss@users.noreply.github.com>
2023-02-27 21:33:50 -0600

    Tidy up PR

--- RNS/Link.py
+++ RNS/Link.py
@@ -22,8 +22,8 @@
 
 from RNS.Cryptography import X25519PrivateKey, X25519PublicKey, Ed25519PrivateKey, Ed25519PublicKey
 from RNS.Cryptography import Fernet
-
 from RNS.Channel import Channel, LinkChannelOutlet
+
 from time import sleep
 from .vendor import umsgpack as umsgpack
 import threading
