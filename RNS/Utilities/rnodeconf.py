93f13a98b2d69734431b101f60bdee81bcc5421c
Dionysis Grigoropoulos <dgrig@erethon.com>
2023-08-14 20:02:00 +0300

    rnodeconf: Dump eeprom under specific directory

--- RNS/Utilities/rnodeconf.py
+++ RNS/Utilities/rnodeconf.py
@@ -237,6 +237,7 @@ try:
     EXT_DIR = CNF_DIR+"/extracted"
     RT_PATH = CNF_DIR+"/recovery_esptool.py"
     TK_DIR  = CNF_DIR+"/trusted_keys"
+    ROM_DIR = CNF_DIR+"/eeprom"
 
     if not os.path.isdir(CNF_DIR):
         os.makedirs(CNF_DIR)
@@ -248,6 +249,8 @@ try:
         os.makedirs(EXT_DIR)
     if not os.path.isdir(TK_DIR):
         os.makedirs(TK_DIR)
+    if not os.path.isdir(ROM_DIR):
+        os.makedirs(ROM_DIR)
 
 except Exception as e:
     print("No access to directory "+str(CNF_DIR)+". This utility needs file system access to store firmware and data files. Cannot continue.")
@@ -2674,7 +2677,7 @@ def main():
                 try:
                     timestamp = time.time()
                     filename = str(time.strftime("%Y-%m-%d_%H-%M-%S"))
-                    path = "./eeprom/"+filename+".eeprom"
+                    path = ROM_DIR + filename + ".eeprom"
                     file = open(path, "wb")
                     file.write(rnode.eeprom)
                     file.close()
