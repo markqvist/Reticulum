b69b939d6fdf157a0547bc0af3a69ab1385f9c3b
Jürg Schulthess <schulthess@puzzle.ch>
2024-04-29 08:10:48 +0200

    realign with upstream

--- Examples/Echo.py
+++ Examples/Echo.py
@@ -341,4 +341,3 @@ if __name__ == "__main__":
     except KeyboardInterrupt:
         print("")
         exit()
-
--- Examples/Link.py
+++ Examples/Link.py
@@ -301,4 +301,3 @@ if __name__ == "__main__":
     except KeyboardInterrupt:
         print("")
         exit()
-
