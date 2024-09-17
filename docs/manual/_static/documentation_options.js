97ae4d74b36ff33e3a1b3708917363b3de0c9223
Mark Qvist <mark@unsigned.io>
2024-09-17 14:56:22 +0200

    Updated docs

--- docs/manual/_static/documentation_options.js
+++ docs/manual/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.7.7 beta',
+    VERSION: '0.7.8 beta',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
