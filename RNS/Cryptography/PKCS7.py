98afe9887000a764dd7af69eecc773f9f1079ade
Mark Qvist <mark@unsigned.io>
2025-03-13 20:11:44 +0100

    Cleanup

--- RNS/Cryptography/PKCS7.py
+++ RNS/Cryptography/PKCS7.py
@@ -1,6 +1,6 @@
 # MIT License
 #
-# Copyright (c) 2022 Mark Qvist / unsigned.io
+# Copyright (c) 2022-2025 Mark Qvist / unsigned.io
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
--- RNS/Utilities/rnodeconf.py
+++ RNS/Utilities/rnodeconf.py
@@ -2,7 +2,7 @@
 
 # MIT License
 #
-# Copyright (c) 2018-2025 Mark Qvist
+# Copyright (c) 2018-2025 Mark Qvist / unsigned.io
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
