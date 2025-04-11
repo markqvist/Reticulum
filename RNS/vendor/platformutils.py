c07e96821872771c21c5b56876030ba4aad3e204
Mark Qvist <mark@unsigned.io>
2025-04-11 12:30:22 +0200

    Cleanup

--- RNS/vendor/platformutils.py
+++ RNS/vendor/platformutils.py
@@ -1,36 +1,26 @@
 def get_platform():
     from os import environ
-    if "ANDROID_ARGUMENT" in environ:
-        return "android"
-    elif "ANDROID_ROOT" in environ:
-        return "android"
+    if "ANDROID_ARGUMENT" in environ: return "android"
+    elif "ANDROID_ROOT" in environ:   return "android"
     else:
         import sys
         return sys.platform
 
 def is_linux():
-    if get_platform() == "linux":
-        return True
-    else:
-        return False
+    if get_platform() == "linux": return True
+    else: return False
 
 def is_darwin():
-    if get_platform() == "darwin":
-        return True
-    else:
-        return False
+    if get_platform() == "darwin": return True
+    else: return False
 
 def is_android():
-    if get_platform() == "android":
-        return True
-    else:
-        return False
+    if get_platform() == "android": return True
+    else: return False
 
 def is_windows():
-    if str(get_platform()).startswith("win"):
-        return True
-    else:
-        return False
+    if str(get_platform()).startswith("win"): return True
+    else: return False
 
 def use_epoll():
     if is_linux() or is_android(): return True
@@ -43,8 +33,7 @@ def use_af_unix():
 def platform_checks():
     if is_windows():
         import sys
-        if sys.version_info.major >= 3 and sys.version_info.minor >= 8:
-            pass
+        if sys.version_info.major >= 3 and sys.version_info.minor >= 8: pass
         else:
             import RNS
             RNS.log("On Windows, Reticulum requires Python 3.8 or higher.", RNS.LOG_ERROR)
@@ -53,7 +42,5 @@ def platform_checks():
 
 def cryptography_old_api():
     import cryptography
-    if cryptography.__version__ == "2.8":
-        return True
-    else:
-        return False
+    if cryptography.__version__ == "2.8": return True
+    else: return False
